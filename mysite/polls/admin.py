70fcd3aa80c06b220976499eb4247b30060498cc
Keyan <Kpesaran@gmail.com>
2024-03-26 11:35:19 -0700

    Set up admin site to add, edit, or delete questions for polls by a superuser

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Question
 # Register your models here.
+
+#need to tell the admin that Question objects have an admin interface
+
+admin.site.register(Question)
