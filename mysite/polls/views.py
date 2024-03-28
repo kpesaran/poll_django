118735e30d09dbe71b508df1f2d4d6d87e3bf55e
Keyan <Kpesaran@gmail.com>
2024-03-27 18:20:17 -0700

    set up a template to display the latest questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from django.template import loader
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    loader.get_template("polls/index.html")
+    context = {"latest_question_list":latest_question_list, }
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse(f"You're voting on question {question_id}")
 
 def example(request, question_id):
     return HttpResponse(f"Look at the url, you will see {question_id}")
